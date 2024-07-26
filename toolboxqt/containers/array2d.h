ac5f64d53e16a4cd93e98166020e0ea73589b64a
Charlie LEGER <c.leger@borea-dental.com>
2024-07-26 10:44:38 +0200

    [containers:array2d] Add doxygen documentation

--- toolboxqt/containers/array2d.h
+++ toolboxqt/containers/array2d.h
@@ -14,6 +14,11 @@ namespace tbq
 /*****************************/
 /* Define template interface */
 /*****************************/
+
+/*!
+ * \class Array2D
+ * \brief Use to manage a 2-dimensional array
+ */
 template <typename T>
 class TOOLBOXQT_EXPORT Array2D
 {
@@ -61,12 +66,24 @@ private:
  *      implementation       */
 /*****************************/
 
+/*!
+ * \brief Construct a 2D array
+ * at specified size
+ *
+ * \param[in] nbRows
+ * Number of rows.
+ * \param[in] nbCols
+ * Number of colums
+ */
 template<typename T>
 Array2D<T>::Array2D(size_t nbRows, size_t nbCols)
 {
     resize(m_rows, m_cols);
 }
 
+/*!
+ * \brief Construct an empty 2D array
+ */
 template<typename T>
 Array2D<T>::Array2D()
     : m_rows(0), m_cols(0)
@@ -74,24 +91,61 @@ Array2D<T>::Array2D()
     /* Nothing to do */
 }
 
+/*!
+ * \brief Get number of rows
+ *
+ * \return
+ * Returns number of rows available
+ *
+ * \sa getCols(), getSize()
+ */
 template<typename T>
 size_t Array2D<T>::getRows() const
 {
     return m_rows;
 }
 
+/*!
+ * \brief Get number of columns
+ *
+ * \return
+ * Returns number of columns available
+ *
+ * \sa getRows(), getSize()
+ */
 template<typename T>
 size_t Array2D<T>::getCols() const
 {
     return m_cols;
 }
 
+/*!
+ * \brief Get total size of 2D array (a.k.a number
+ * of elements)
+ *
+ * \return
+ * Returns number of elements
+ *
+ * \sa getRows(), getCols()
+ */
 template<typename T>
 size_t Array2D<T>::getSize() const
 {
     return m_rows * m_cols;
 }
 
+/*!
+ * \brief Clear content of 2D array
+ * \details
+ * Size will be reset to \c 0 and all elements will
+ * be removed
+ *
+ * \note
+ * This method doesn't impact internal capacity of
+ * the array, see \c resize() method for more details.
+ *
+ * \sa resize()
+ */
 template<typename T>
 void Array2D<T>::clear()
 {
@@ -100,6 +154,18 @@ void Array2D<T>::clear()
     m_data.clear();
 }
 
+/*!
+ * \brief Resize array to specified size
+ *
+ * \param[in] nbRows
+ * Number of rows.
+ * \param[in] nbCols
+ * Number of colums
+ *
+ * \note
+ * This method will only extend \b capacity of the 2D array,
+ * no shrink operation will be performed.
+ */
 template<typename T>
 void Array2D<T>::resize(size_t nbRows, size_t nbCols)
 {
@@ -108,18 +174,57 @@ void Array2D<T>::resize(size_t nbRows, size_t nbCols)
     m_data.resize(m_rows * m_cols);
 }
 
+/*!
+ * \brief Use to insert a value at specified
+ * indexes.
+ *
+ * \param[in] row
+ * Row index to use. \n
+ * Must be valid (i.e <tt>0 <= row < getRows()</tt>).
+ * \param[in] col
+ * Column index to use. \n
+ * Must be valid (i.e <tt>0 <= col < getCols()</tt>).
+ * \param[in] value
+ * Value to insert
+ */
 template<typename T>
 void Array2D<T>::insert(size_t row, size_t col, const T &value)
 {
     (*this)(row, col) = value;
 }
 
+/*!
+ * \brief Get modifiable reference to an element
+ *
+ * \param[in] row
+ * Row index to use. \n
+ * Must be valid (i.e <tt>0 <= row < getRows()</tt>).
+ * \param[in] col
+ * Column index to use. \n
+ * Must be valid (i.e <tt>0 <= col < getCols()</tt>).
+ *
+ * \return
+ * Returns modifiable reference to an element
+ */
 template<typename T>
 T& Array2D<T>::operator()(size_t row, size_t col)
 {
     return m_data[row * m_cols + col];
 }
 
+/*!
+ * \brief Get constant reference to an element
+ *
+ * \param[in] row
+ * Row index to use. \n
+ * Must be valid (i.e <tt>0 <= row < getRows()</tt>).
+ * \param[in] col
+ * Column index to use. \n
+ * Must be valid (i.e <tt>0 <= col < getCols()</tt>).
+ *
+ * \return
+ * Returns constant reference to an element
+ */
 template<typename T>
 const T& Array2D<T>::operator()(size_t row, size_t col) const
 {
