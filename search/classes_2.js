7f0e283d70d67749d555c1feee02382fd2b3798d
legerch <54318446+legerch@users.noreply.github.com>
2025-11-12 12:00:08 +0000

    Deploying to gh-pages from @ legerch/ToolBoxQt@ba71291e1b36a6eed81239f952d95618d9b24fd4 🚀

--- search/classes_2.js
+++ search/classes_2.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['corehelper_0',['CoreHelper',['../classtbq_1_1_core_helper.html',1,'tbq']]]
+  ['colorbutton_0',['ColorButton',['../classtbq_1_1_color_button.html',1,'tbq']]],
+  ['corehelper_1',['CoreHelper',['../classtbq_1_1_core_helper.html',1,'tbq']]]
 ];
